e9c75c5f7b1ad41a29dae9862827389084a1d5df
Eyyub Saral <eyyubsrl04@gmail.com>
2025-03-08 11:09:09 +0300

    acordion

--- Accordion.js
+++ Accordion.js
@@ -10,42 +10,45 @@ export default function Accordion() {
   }
 
   return (
-    <div className="accordion">
-      <div className="accordion-item">
-        <h2 className="accordion-header" onClick={() => handleClick(0)}>
-          Ürün kaliteli mi?
-        </h2>
-        {activeIndex === 0 && (
-          <p className="accordion-content">
-            Kaliteli mi? Eh, kendisi tam olarak bir Antik Mısır eseri kadar eski
-            görünmüyor ama kalite konusunda uzaktan akraba olabilirler. Yine de
-            iş görür... belki.
-          </p>
-        )}
-      </div>
-      <div className="accordion-item">
-        <h2 className="accordion-header" onClick={() => handleClick(1)}>
-          Fiyatı ne kadar?
-        </h2>
-        {activeIndex === 1 && (
-          <p className="accordion-content">
-            Fiyat? Şaka yapmıyorsunuz, değil mi? Hani, cüzdanınıza bakıp "Bu aşk
-            bizi iflasa sürükler" dedirtecek cinsten. Ama unutmayın, pahalı her
-            zaman iyidir demek değildir... Genelde değildir.
-          </p>
-        )}
-      </div>
-      <div className="accordion-item">
-        <h2 className="accordion-header" onClick={() => handleClick(2)}>
-          Ne Zaman Ulaşılır
-        </h2>
-        {activeIndex === 2 && (
-          <p className="accordion-content">
-            Teslimat süremiz? Hmm... Belki bu siparişi veren kişi bir sonraki
-            nesil siz olabilirsiniz! Ama sabırlı olun, bir gün mutlaka gelir…
-            belki.
-          </p>
-        )}
+    <div className="main-accordion">
+      <h2 className="main-head">Sıkça Sorulan Sorular</h2>
+      <div className="accordion">
+        <div className="accordion-item">
+          <h2 className="accordion-header" onClick={() => handleClick(0)}>
+            Ürün kaliteli mi?
+          </h2>
+          {activeIndex === 0 && (
+            <p className="accordion-content">
+              Kaliteli mi? Eh, kendisi tam olarak bir Antik Mısır eseri kadar
+              eski görünmüyor ama kalite konusunda uzaktan akraba olabilirler.
+              Yine de iş görür... belki.
+            </p>
+          )}
+        </div>
+        <div className="accordion-item">
+          <h2 className="accordion-header" onClick={() => handleClick(1)}>
+            Fiyatı ne kadar?
+          </h2>
+          {activeIndex === 1 && (
+            <p className="accordion-content">
+              Fiyat? Şaka yapmıyorsunuz, değil mi? Hani, cüzdanınıza bakıp "Bu
+              aşk bizi iflasa sürükler" dedirtecek cinsten. Ama unutmayın,
+              pahalı her zaman iyidir demek değildir... Genelde değildir.
+            </p>
+          )}
+        </div>
+        <div className="accordion-item">
+          <h2 className="accordion-header" onClick={() => handleClick(2)}>
+            Ne Zaman Ulaşılır
+          </h2>
+          {activeIndex === 2 && (
+            <p className="accordion-content">
+              Teslimat süremiz? Hmm... Belki bu siparişi veren kişi bir sonraki
+              nesil siz olabilirsiniz! Ama sabırlı olun, bir gün mutlaka gelir…
+              belki.
+            </p>
+          )}
+        </div>
       </div>
     </div>
   );
